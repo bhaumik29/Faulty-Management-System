4e63ddbff22c60ea27763a36b7a685c25662e9a5
Himilsinh Sindha <102691151+himilsinh-sindha@users.noreply.github.com>
2024-03-27 00:43:20 +0530

    Update BackendApplication.java

--- src/main/java/me/csproconnect/backend/BackendApplication.java
+++ src/main/java/me/csproconnect/backend/BackendApplication.java
@@ -9,5 +9,5 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-	System.out.println("Hello world !!");
+	System.out.println("Hello Bhaumik !!");
 }
